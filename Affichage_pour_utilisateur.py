14a970d8ad00b7824137e80ed662d58a0c750413
syrine <syrine.boudef.1@ens.etsmtl.ca>
2025-05-29 01:30:03 -0400

    Modifications de noms de quelques fonctions.

--- Affichage_pour_utilisateur.py
+++ Affichage_pour_utilisateur.py
@@ -1,4 +1,4 @@
-from cesar import chiffrement_cesar, dechiffrement_cesar, bruteforce
+from cesar import*
 from utils import lire_fichier, ecrire_fichier
 
 def demander_texte():
@@ -26,43 +26,42 @@ def demander_sortie():
 def menu_principal():
     while True:
         print("\n--- Menu Principal ---")
-        print("1. Chiffrer un message")
-        print("2. Déchiffrer un message")
+        print("1. Chiffrer un texte")
+        print("2. Déchiffrer un texte")
         print("3. Chiffrer un fichier")
         print("4. Déchiffrer un fichier")
-        print("5. Décrypter sans clé (Brute force)")
         print("6. Quitter")
 
         choix = input("Votre choix : ")
 
         if choix == '1':
-            message = demander_texte()
+            texte = demander_texte()
             cle = demander_cle()
             sortie = demander_sortie()
             if sortie == '1':
-                print("\nMessage chiffré :", chiffrement_cesar(message, cle))
+                print("\nTexte chiffré :", chiffrement_texte(texte, cle))
             else:
                 nom_fichier = input("Nom du fichier de sortie : ")
                 nom_fichier =f"{nom_fichier}.txt"
-                ecrire_fichier(nom_fichier, chiffrement_cesar(message, cle))
+                ecrire_fichier(nom_fichier, chiffrement_texte(texte, cle))
                 print(f"Résultat enregistré dans {nom_fichier}")
         elif choix == '2':
-            message = demander_texte()
+            texte = demander_texte()
             cle = demander_cle()
             sortie = demander_sortie()
             if sortie == '1':
-                print("\nMessage déchiffré :", dechiffrement_cesar(message, cle))
+                print("\nTexte déchiffré :", dechiffrement_texte(texte, cle))
             else:
                 nom_fichier = input("Nom du fichier de sortie : ")
                 nom_fichier = f"{nom_fichier}.txt"
-                ecrire_fichier(nom_fichier, dechiffrement_cesar(message, cle))
+                ecrire_fichier(nom_fichier, dechiffrement_texte(texte, cle))
                 print(f"Résultat enregistré dans {nom_fichier}")
         elif choix == '3':
             nom = input("Nom du fichier à chiffrer : ")
             contenu = lire_fichier(nom)
             if contenu:
                 cle = demander_cle()
-                resultat = chiffrement_cesar(contenu, cle)
+                resultat = chiffrement_texte(contenu, cle)
                 nom_sortie = input("Nom du fichier de sortie (ex: mon_fichier_chiffre.txt) : ")
                 ecrire_fichier(nom_sortie, resultat)
                 print(f"Fichier chiffré enregistré dans {nom_sortie}")
@@ -71,13 +70,11 @@ def menu_principal():
             contenu = lire_fichier(nom)
             if contenu:
                 cle = demander_cle()
-                resultat = dechiffrement_cesar(contenu, cle)
+                resultat = dechiffrement_texte(contenu, cle)
                 nom_sortie = input("Nom du fichier de sortie (ex: mon_fichier_dechiffre.txt) : ")
                 ecrire_fichier(nom_sortie, resultat)
                 print(f"Fichier déchiffré enregistré dans {nom_sortie}")
-        elif choix == '5':
-            message = demander_texte()
-            bruteforce(message)
+
         elif choix == '6':
             print("Au revoir !")
             break
--- cesar.py
+++ cesar.py
@@ -4,40 +4,40 @@ from charset_normalizer.utils import remove_accent
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def chiffrement_cesar(message, cle):
-    message_sans_accent = enlever_caracteres_speciaux(message)
-    message_crypte = ''
+def chiffrement_texte(texte, cle):
+    texte_sans_accent = enlever_caracteres_speciaux(texte)
+    texte_crypte = ''
     # Convertir en minuscule pour gérer les majuscules
-    for lettre in message_sans_accent.lower():
+    for lettre in texte_sans_accent.lower():
         # Crypter que les lettres de l'alphabet
         if lettre in alphabet:
             l = alphabet.find(lettre)
             l = (l + cle) % 26  # %26 assure que l'indice reste entre 0 et 25
-            message_crypte += alphabet[l]
+            texte_crypte += alphabet[l]
         else:
             # Garder les caractères non alphabétiques (espaces, ponctuation, etc)
-            message_crypte += lettre
+            texte_crypte += lettre
 
-    return message_crypte
+    return texte_crypte
 
-def dechiffrement_cesar(message, cle):
-    message_sans_accent = enlever_caracteres_speciaux(message)
-    message_decrypte = ''
-    for lettre in message_sans_accent.lower():
+def dechiffrement_texte(texte, cle):
+    texte_sans_accent = enlever_caracteres_speciaux(texte)
+    texte_decrypte = ''
+    for lettre in texte_sans_accent.lower():
         if lettre in alphabet:
             l = alphabet.find(lettre)
             l = (l - cle) % 26  # On soustrait k
-            message_decrypte += alphabet[l]
+            texte_decrypte += alphabet[l]
         else:
             # Garde les caractères non alphabétiques
-            message_decrypte += lettre
+            texte_decrypte += lettre
 
-    return message_decrypte
+    return texte_decrypte
 
-def bruteforce(message):
+def bruteforce(texte):
     print("\n--- Mode Brute Force ---")
     for cle in range(1, 26):
-        tentative = dechiffrement_cesar(message, cle)
+        tentative = dechiffrement_texte(texte, cle)
         print(f"Clé {cle}: {tentative}")
     print("Essayez d’identifier visuellement le bon résultat.")
 
