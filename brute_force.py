cd63b7828618fcdefb95554d7d1fc7aa2a414303
syrine <syrine.boudef.1@ens.etsmtl.ca>
2025-05-29 01:45:52 -0400

    Ajout d'une fonction qui sert a tester les cles et les valider.

--- brute_force.py
+++ brute_force.py
@@ -6,7 +6,7 @@ def compter_mots_frequents(texte, mots_frequents):
     mots = texte.lower().translate(str.maketrans('', '', string.punctuation)).split()
     return sum(1 for mot in mots if mot in mots_frequents)
 
-# Fonction pour tester les cles et les valider
+# Fonction pour tester les cles et les valider.
 def tester_cles_et_valider(texte, cles_a_tester, dictionnaire):
     scores = []
     for cle in cles_a_tester:
@@ -14,3 +14,16 @@ def tester_cles_et_valider(texte, cles_a_tester, dictionnaire):
         mots = texte_dechiffre.lower().translate(str.maketrans('', '', string.punctuation)).split()
         score = sum(1 for mot in mots if mot in dictionnaire)
         scores.append((cle, texte_dechiffre, score))
+# Trier les résultats par score décroissant
+    scores.sort(key=lambda x: x[2], reverse=True)
+
+    for cle, texte_propose, score in scores:
+        print(f"\n Proposition avec la clé {cle} (score: {score}) :")
+        print(texte_propose)
+        reponse = input(" Ce texte vous semble correct ? (o/n) : ").strip().lower()
+        if reponse == 'o':
+            print("Texte validé par l'utilisateur.")
+            return texte_propose, cle
+
+    print(" Aucun texte n'a été validé par l'utilisateur ")
+    return None, None
