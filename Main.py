c089b27081c493929e70c716d56471b4b6ab13ac
wilson <wilson-david.parra-oliveros.1@ens.etsmtl.ca>
2025-05-29 20:20:06 -0400

    creation de l en-tete du script principal

--- Main.py
+++ Main.py
@@ -1,16 +1,21 @@
-1# Chiffrement de cesar
-#print('Bienvenue dans le programme de chiffrement de cesar')
-#choix_utilisateur = print(input('voulez-vous crypter ou decrypter du texte ?'))
-# fonction qui sert au cryptage
-#if choix_utilisateur=='crypter':
-    #Boucle
+"""
+Programme de chiffrement et déchiffrement avec le chiffrement de César
+-----------------------------------------------------------------------
 
-#else
-# fonction qui sert au decryptage
-   #Boucle
-#end
+Ce programme permet à l'utilisateur de :
+- Chiffrer un message avec une clé (positive ou négative)
+- Déchiffrer un message avec une clé connue
+- Appliquer un déchiffrement automatique (brute force) sans connaître la clé
+- Chiffrer ou déchiffrer le contenu d’un fichier texte
+- Enregistrer les résultats dans un fichier ou les afficher à l’écran
 
-# dernier option
+Modules requis : cesar.py (algorithmes de chiffrement), utils.py (lecture/écriture de fichiers)
+
+Auteurs : Alexis Chenuet
+          Syrine Boudef
+          Wilson David Parra Oliveros
+"""
+
 from Affichage_pour_utilisateur import menu_principal
 
 menu_principal()
