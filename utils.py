73ad95db6082e8df2a5d3a102ac40d660ce30004
wilson <wilson-david.parra-oliveros.1@ens.etsmtl.ca>
2025-05-29 20:25:29 -0400

    Ajout des commentaires explicatives des functions
    A

--- utils.py
+++ utils.py
@@ -6,19 +6,26 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 # Fonction pour enlever les caracteres speciaux
 def enlever_caracteres_speciaux(texte):
+    # Normalise le texte en forme 'NFKD', ce qui sépare les lettres des accents
     normalized_text = unicodedata.normalize('NFKD', texte)
+
+    # Recompose le texte en supprimant les caractères "diacritiques" (accents, etc.)
     return ''.join([char for char in normalized_text if not unicodedata.combining(char)])
 
 # Fonction pour lire un fichier
 def lire_fichier(nom_fichier):
     try:
+        # Ouvre le fichier en mode lecture texte avec l'encodage UTF-8
         with open(nom_fichier, 'r', encoding='utf-8') as f:
             return f.read()
     except FileNotFoundError:
+        # Gère l'erreur si le fichier n'existe pas
         print(f"Fichier {nom_fichier} introuvable.")
         return None
+
 # Fonction pour ecrire un fichier
 def ecrire_fichier(nom_fichier, contenu):
+    # Ouvre le fichier en mode écriture avec l'encodage UTF-8
     with open(nom_fichier, 'w', encoding='utf-8') as f:
         f.write(contenu)
 
@@ -54,4 +61,3 @@ def demander_sortie():
         if choix in ('1', '2'):
             return choix
         print("Choix invalide.")
-
